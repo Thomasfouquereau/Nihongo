25a9198bfd1b4e89ec68ba00667e60e188f26ea8
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-08-26 17:29:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+
+
+  return (
+    
+    <Routes>
+
+      <Route path="/" element={<h1>Home</h1>} />
+
+    </Routes>
+
+  )
+}
+
+export default App
