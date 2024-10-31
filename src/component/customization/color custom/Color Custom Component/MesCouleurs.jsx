76b216d3b6a52192e10905edba61047fee4814e4
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-10-31 16:50:13 +0100

    Refactoriser le composant MesCouleurs pour améliorer la personnalisation des couleurs

--- src/component/customization/color custom/Color Custom Component/MesCouleurs.jsx
+++ src/component/customization/color custom/Color Custom Component/MesCouleurs.jsx
@@ -3,27 +3,108 @@ import { useState } from 'react';
 import { setColor } from '../../../store';
 import styled from 'styled-components';
 
+const ColorCutomTitle = styled.div`
+    font-size: 1.5vw;
+    background-color: ${props => props.$bgColor};
+    border-radius:  0.8vw 0.8vw 0 0 ;
+    padding: 1vw;
+    padding-bottom: 0;
+    width: 15vw;
+    height: 3vw;
+    position: relative; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span{
+        background-color: ${props => props.$mainBgColor};
+        color: ${props => props.$fontColor};
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius:  0.5vw;
+        font-weight: 700;
+        text-transform: uppercase;
+    }
+    &::after {
+        content: '';
+        position: absolute;
+        background-color: ${props => props.$bgColor};
+        width: 2vw;
+        height: 2vw;
+        right: -2vw;
+        bottom:0;
+    }
+    &::before {
+        content: '';
+        position: absolute;
+        background-color: ${props => props.$mainBgColor};
+        width: 2vw;
+        height: 2vw;
+        right: -2vw;
+        bottom:0;
+        z-index: 1;
+        border-radius:  0vw 0vw 0vw 0.8vw ;
+    }
+`
+
 const ColorCutomContainer = styled.div`
-width: 10vw;
-height: 10vw;`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center ;
+    gap: 1vw;
+    background-color: ${props => props.$bgColor};
+    width: 92vw;
+    padding: 1vw;
+    border-radius: 0 0.8vw 0.8vw 0.8vw ;
+`
+
+const ColorCutomItemCadre = styled.div`
+    padding: 0.8vw;
+    background-color: ${props => props.$mainBgColor};
+    border-radius: 0.8vw;
+    transition: transform 0.3s;
+    will-change: transform;
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.07);
+    }
+`
+
+const ColorCutomItem = styled.div`
+    width: 10vw;
+    height: 10vw;
+    border-radius: 0.5vw ;
+`
 
 const MesCouleurs = () => {
     const mesCouleurs = useSelector(state => state.colorCustom.mesCouleurs);
+    const { fontColor, mainBgColor, bgColor } = useSelector((state) => state.mode);
     const dispatch = useDispatch();
     const { color } = useSelector((state) => state.color);
     const [, setColorsState] = useState(color);
-
     const handleColorChange = (newColor) => () => {
         setColorsState(newColor);
         dispatch(setColor(newColor));
     };
-    
+
     return (
         <div>
-            {mesCouleurs.map((couleur, index) => (
-                <ColorCutomContainer key={index} style={{ backgroundColor: couleur }} onClick={handleColorChange(couleur)}>
-                </ColorCutomContainer>
-            ))}
+            <ColorCutomTitle $fontColor={fontColor} $bgColor={bgColor} $mainBgColor={mainBgColor}>
+                <span>
+                    mes couleurs
+                </span>
+            </ColorCutomTitle>
+            <ColorCutomContainer $bgColor={bgColor} >
+                {mesCouleurs.map((couleur, index) => (
+                    <ColorCutomItemCadre key={index} $mainBgColor={mainBgColor} >
+                        <ColorCutomItem style={{ backgroundColor: couleur }} onClick={handleColorChange(couleur)}>
+                        </ColorCutomItem>
+                    </ColorCutomItemCadre>
+
+                ))}
+            </ColorCutomContainer>
         </div>
     );
 };
--- src/component/customization/color custom/ColorCustom.jsx
+++ src/component/customization/color custom/ColorCustom.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setColor } from '../../store';
 import styled from 'styled-components';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -41,65 +38,15 @@ const ColorCutomContainer = styled.div`
     }
 `
 
-// const ButtonContainer = styled.div`
-//     display: flex;
-//     flex-wrap: wrap;
-//     justify-content: center;
-//     align-items: center;
-//     margin-top: 2vw;
-//     width: 100%;
-//     @media screen and (max-width: 560px) {
-//         margin-top: 10vw;
-//     }
-// `
-
-const Button = styled.button`
-    width: 30%;
-    height: 15vw;
-    border: 1vw solid ${(props) => props.$bgColor};
-    border-radius: 0.8vw;
-    cursor: pointer;
-    transition: cubic-bezier(0.075, 0.82, 0.165, 1) 0.7s;
-    will-change: transform;
-    &:hover{
-        transform: scale(1.1);
-    }
-    @media screen and (max-width: 560px) {
-        width: 45%;
-        height: 30vw;
-    }
-`
-
 export default function ColorCustom() {
-    const dispatch = useDispatch();
     const { color } = useSelector((state) => state.color);
     const { bgColor, fontColor } = useSelector((state) => state.mode);
-    const [, setColorsState] = useState(color);
-
-    const handleColorChange = (newColor) => {
-        setColorsState(newColor);
-        dispatch(setColor(newColor));
-    };
-
-
 
     let navigate = useNavigate();
     return (
         <ColorCutomContainer $fontColor={fontColor} $bgColor={bgColor}>
             <Link style={{ backgroundColor: color }} onClick={() => navigate(-1)}>Retour</Link>
-            <div>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#F75D48')} style={{ backgroundColor: '#F75D48' }}></Button>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#5448F7')} style={{ backgroundColor: '#5448F7' }}></Button>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#FF0000')} style={{ backgroundColor: '#FF0000' }}></Button>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#42e55b')} style={{ backgroundColor: '#42e55b' }}></Button>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#1ce8c2')} style={{ backgroundColor: '#1ce8c2' }}></Button>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#FF4BC9')} style={{ backgroundColor: '#FF4BC9' }}></Button>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#48B4F7')} style={{ backgroundColor: '#48B4F7' }}></Button>
-                <Button $bgColor={bgColor} onClick={() => handleColorChange('#08a2b7')} style={{ backgroundColor: '#08a2b7' }}></Button>
-            </div>
             <MesCouleurs />
         </ColorCutomContainer>
     );
 }
-
-{/* */ }
