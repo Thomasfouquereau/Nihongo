7b0d6dbaacfd0ada8e6b4973f4af11de5b039df3
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-11-11 11:26:11 +0100

    Mettre à jour le contenu des articles avec des textes enrichis et améliorer le style du composant ArticleContent pour une meilleure présentation.

--- src/component/main/articles/article/component/ArticleContent.jsx
+++ src/component/main/articles/article/component/ArticleContent.jsx
@@ -2,15 +2,56 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const Container = styled.div``
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1vw;
+    width: calc( 100% - 6vw);
+`
 
-const ArticleContentCreator = ({ heading, content, type, list, image, content1, content2 }) => {
+const Paragraphs = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1vw;
+    background-color: ${(props) => props.$bgColor};
+    color: ${(props) => props.$fontColor};
+    padding: 1vw;
+    border-radius: 0.8vw;
+    width: calc(  100% - 2vw);
+    div{
+        display: flex;
+        flex-direction: column;
+        gap: 1vw;
+        width: 100%;
+        h2{
+            background-color: ${(props) => props.$mainBgColor};
+            color: ${(props) => props.$color};
+            border-radius: 0.5vw;
+            padding: 1vw 2vw;
+            font-size: 2vw;  
+        }
+        p{
+            font-size: 1.2vw;
+            background-color: ${(props) => props.$mainBgColor};
+            border-radius: 0.5vw;
+            padding: 2vw;
+            line-height: 1.4 ;
+            @media screen and (max-width: 560px){
+                font-size: 4vw;
+            }
+        }
+    }
+`
+
+const ArticleContentCreator = ({ heading, content, type, list, image, content1, content2, bgColor, fontColor, mainBgColor, color }) => {
     if (type === "paragraphs") {
         return (
-            <div>
-                <h2>{heading}</h2>
-                <p>{content}</p>
-            </div>
+            <Paragraphs $bgColor={bgColor} $fontColor={fontColor} $mainBgColor={mainBgColor} $color={color}>
+                <div>
+                    <h2>{heading}</h2>
+                    <p>{content}</p>
+                </div>
+            </Paragraphs>
         )
     } else if (type === "list and image") {
         return (
@@ -18,7 +59,7 @@ const ArticleContentCreator = ({ heading, content, type, list, image, content1, 
                 <h2>{heading}</h2>
                 {list && list.map((item, index) => (
                     <ul key={index}>
-                        <li>{item.name}</li>
+                        <p>{item.name}</p>
                         {item.itemList.map((subItem, subIndex) => (
                             <li key={subIndex}>{subItem.item}</li>
                         ))}
@@ -37,18 +78,20 @@ const ArticleContentCreator = ({ heading, content, type, list, image, content1, 
         )
     } else if (type === "paragraphs and paragraphs") {
         return (
-            <div>
-                <h2>{heading}</h2>
-                <p>{content1}</p>
-                <p>{content2}</p>
-            </div>
+            <Paragraphs $bgColor={bgColor} $fontColor={fontColor} $mainBgColor={mainBgColor} $color={color}>
+                <div>
+                    <h2>{heading}</h2>
+                    <p>{content1}</p>
+                    <p>{content2}</p>
+                </div>
+            </Paragraphs>
         )
     }
     return (
-        <div>
+        <Paragraphs $bgColor={bgColor} $fontColor={fontColor} $mainBgColor={mainBgColor} $color={color}>
             <h2>{heading}</h2>
             <p>{content}</p>
-        </div>
+        </Paragraphs>
     )
 }
 
@@ -68,11 +111,18 @@ ArticleContentCreator.propTypes = {
     ),
     image: PropTypes.string,
     content1: PropTypes.string,
-    content2: PropTypes.string
+    content2: PropTypes.string,
+    bgColor: PropTypes.string,
+    fontColor: PropTypes.string,
+    mainBgColor: PropTypes.string,
+    color: PropTypes.string
 }
 
 export default function ArticleContent({ article }) {
-    const { bgColor } = useSelector((state) => state.mode);
+
+    const { bgColor, fontColor, mainBgColor } = useSelector((state) => state.mode);
+    const { color } = useSelector((state) => state.color);
+
     return (
         <Container $bgColor={bgColor}>
             {article.paragraphs.map((paragraph, index) => (
@@ -85,6 +135,10 @@ export default function ArticleContent({ article }) {
                     image={paragraph.image}
                     content1={paragraph.content1}
                     content2={paragraph.content2}
+                    bgColor={bgColor}
+                    fontColor={fontColor}
+                    mainBgColor={mainBgColor}
+                    color={color}
                 />
             ))}
         </Container>
